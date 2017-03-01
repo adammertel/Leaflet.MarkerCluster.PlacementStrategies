ac8caa5e377b2eec7fc3caf2d9d4d3619d903b25
adammertel <mertel.adam@gmail.com>
2017-03-01 20:21:49 +0100

    naming

--- src/markercluster.strategies.js
+++ src/markercluster.strategies.js
@@ -1,6 +1,3 @@
-//This code is 100% based on https://github.com/jawj/OverlappingMarkerSpiderfier-Leaflet
-//Huge thanks to jawj for implementing it first to make my job easy :-)
-
 L.MarkerCluster.include({
 
 	spiderfy: function () {
